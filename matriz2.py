26a37860f495b19d9c7b3aec71c4e692c5629e15
Victoriafcz <159342439+Victoriafcz@users.noreply.github.com>
2024-02-14 11:28:42 -0800

    Add files via upload

---
+++ matriz2.py
@@ -0,0 +1,18 @@
+#hacer un programa que lea y muestre uno por uno los elementos de una matriz 2x3
+matriz2=[[0,0,0],[0,0,0]]
+
+matriz2[0][0] =int(input("dame el elemento [0][0]: "))
+matriz2[0][1] =int(input("dame el elemento [0][1]: "))
+matriz2[0][2] =int(input("dame el elemento [0][2]: "))
+matriz2[0][0] =int(input("dame el elemento [0][0]: "))
+matriz2[0][1] =int(input("dame el elemento [0][1]: "))
+matriz2[0][2] =int(input("dame el elemento [0][2]: "))
+
+print("el elemento [0] [0] es ", matriz2 [0][0])
+print("el elemento [0] [1] es ", matriz2 [0][1])
+print("el elemento [0] [2] es ", matriz2 [0][2])
+print("el elemento [0] [0] es ", matriz2 [0][0])
+print("el elemento [0] [1] es ", matriz2 [0][1])
+print("el elemento [0] [2] es ", matriz2 [0][2])
+
+print(matriz2)
